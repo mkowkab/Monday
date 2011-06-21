cf67a1e1ea58e535703ea11cfa97509cff4f9135
Muqeeth_Kowkab <Muqeeth_Kowkab@WN7X64-C9TYSM1.aus.amer.dell.com>
2011-06-21 10:25:03 -0500

    Change-Id: Ifdc0b174c64e094f9ed453ad7747457cd28c9319

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 
 public class GitTest {
          public static void main(String[] args){
-        	 System.out.println("Githubbbbbbbbbbbbbb");
+        	 System.out.println("Githubbb");
          }
 }
